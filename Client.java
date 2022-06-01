b085f157330258e38afbfcc5aa2eb227d5584f31
Abdullah-Gohar <abdullahgohar01@gmail.com>
2022-06-01 15:48:00 +0500

    Added Socket Output Closing

--- Client.java
+++ Client.java
@@ -8,19 +8,28 @@ public class Client {
         Socket socket = null;
         FileInputStream fin = null;
         DataOutputStream dout = null;
+        DataInputStream din = null;
+        ObjectInputStream in;
         try {
             socket = new Socket("localhost",8080);
             dout = new DataOutputStream(socket.getOutputStream());
-            File file = new File("D:\\CN\\Project\\data1.txt");
+            din = new DataInputStream(socket.getInputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+            File file = new File("D:\\CN\\Project\\data1.pdf");
             fin = new FileInputStream(file);
             byte[] buffer = new byte[4 * 1024];
             int count;
             while ((count = fin.read(buffer)) > 0) {
                 dout.write(buffer, 0, count);
             }
+            socket.shutdownOutput();
+            System.out.println("Loop Exit");
+            System.out.println((String) in.readObject());
+            System.out.println("Got it!");
             socket.close();
         } catch (Exception e) {
-
+            System.out.println(e);
+            System.out.println("Exception!");
         }
     }
 }
--- Server.java
+++ Server.java
@@ -8,22 +8,31 @@ class Server{
         Socket client = null;
         FileOutputStream fout = null;
         DataInputStream din  = null;
+        DataOutputStream dout = null;
+        ObjectOutputStream out  = null;
         try{
             server = new ServerSocket(8080);
             client = server.accept();
+            System.out.println("Files");
             din = new DataInputStream(client.getInputStream());
-            File file = new File("D:\\CN\\Project\\data.txt");
+            dout = new DataOutputStream(client.getOutputStream());
+            out = new ObjectOutputStream(client.getOutputStream());
+            File file = new File("D:\\CN\\Project\\data.pdf");
             fout = new FileOutputStream(file);
             byte[] buffer = new byte[4*1024];
             int count;
             while((count = din.read(buffer))>0){
                 fout.write(buffer,0,count);
             }
+            System.out.println("Files");
+            String str = "File Recieved!";
+            out.writeObject(str);
+            System.out.println("Files2");
             client.close();
             server.close();
         }
         catch(Exception e){
-
+            System.out.println("Exception!");
         }
     }
 }
