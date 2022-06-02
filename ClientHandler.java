f678225c49975732bd3855fdab0da1b44a022cf9
Abdullah-Gohar <abdullahgohar01@gmail.com>
2022-06-02 16:52:22 +0500

    Added DB

--- ClientHandler.java
+++ ClientHandler.java
@@ -23,12 +23,15 @@ public class ClientHandler implements Runnable{
             }
             File file = new File("D:\\CN\\Project\\"+name);
 
-            
+
             FileOutputStream fout = new FileOutputStream(file);
-            byte[] buffer = new byte[4 * 1024];
+            byte[] buffer = new byte[1024 * 1024];
             int count;
+            int i =0;
             while ((count = din.read(buffer)) > 0) {
                 fout.write(buffer, 0, count);
+                i++;
+                System.out.println(i);
             }
             System.out.println("Files");
             String str = "File Recieved!";
---
+++ DBConn.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author kabeer
+ */
+public class DBConn {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:sqlserver://DESKTOP-TA4RQON:1433;databaseName=Resort_DB;userName=admin;password=123;trustServerCertificate=true");
+            PreparedStatement statement = connection.prepareStatement("select * from Client");
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                System.out.print(result.getString("ClientFirstName") + " ");
+                System.out.print(result.getString("ClientLastName") + " ");
+                System.out.println(result.getString("Gender") + " ");
+            }
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
