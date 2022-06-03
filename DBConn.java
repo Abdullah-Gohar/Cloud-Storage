209407962e7dccdaab24d64d9cab602c2ad1de86
mishaltariq <tariqmishal475@gmail.com>
2022-06-03 15:32:03 +0500

    message\

--- DBConn.java
+++ DBConn.java
@@ -12,7 +12,7 @@ public class DBConn {
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection(
-                    "jdbc:sqlserver://DESKTOP-TA4RQON:1433;databaseName=Resort_DB;userName=admin;password=123;trustServerCertificate=true");
+                 //   "jdbc:sqlserver://DESKTOP-5CB18B9:1433;databaseName=Resort_DB;userName=mishal123;password=123;trustServerCertificate=true");
             PreparedStatement statement = connection.prepareStatement("select * from Client");
             ResultSet result = statement.executeQuery();
             while (result.next()) {
