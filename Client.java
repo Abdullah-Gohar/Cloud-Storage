2257f3960790ec60e8d98ce37577c779dcba533f
Bear Hands <ahareem134@gmail.com>
2022-06-05 11:16:04 +0500

    Login branch

--- Client.java
+++ Client.java
@@ -11,20 +11,44 @@ public class Client {
         DataInputStream din = null;
         ObjectInputStream in;
         ObjectOutputStream out;
+        Scanner sc= new Scanner(System.in);
         try {
             socket = new Socket("localhost",8085);
+
             dout = new DataOutputStream(socket.getOutputStream());
             din = new DataInputStream(socket.getInputStream());
             in = new ObjectInputStream(socket.getInputStream());
             out = new ObjectOutputStream(socket.getOutputStream());
-            File file = new File("D:\\CN\\Project\\data1.pdf");
-            System.out.println("Enter Username: ");
-            Scanner scn = new Scanner(System.in);
-            System.out.println("Enter Password: ");
+            int choice;
+            System.out.println("Enter 0 for Registeration and enter 1 to login");
+            choice= sc.nextInt();
+            if(choice==0) {
+                String userName;
+                String pass;
+                String purpose;
+                String email;
+                System.out.println("Enter user name");
+                userName= sc.next();
+                System.out.println("Enter password");
+                pass= sc.next();
+                System.out.println("Enter email");
+                email= sc.next();
+                System.out.println("Enter purpose");
+                purpose= sc.next();
+                String[] data= {userName, pass,email,purpose};
+                // clientData Clientdata= new clientData(userName, pass, purpose);
+                // //dout=new DataOutputStream(socket.getOutputStream());
+                // out.writeObject(Clientdata);
+                out.writeObject(data);
+            }
+            /*File file = new File("D:\\CN\\Project\\data1.pdf");
+            // System.out.println("Enter Username: ");
+            // Scanner scn = new Scanner(System.in);
+            // System.out.println("Enter Password: ");
             
             fin = new FileInputStream(file);
 
-            scn.nextLine();
+            //scn.nextLine();
             byte[] buffer = new byte[4 * 1024];
             int count;
             while ((count = fin.read(buffer)) > 0) {
@@ -33,7 +57,7 @@ public class Client {
             socket.shutdownOutput();
             System.out.println("Loop Exit");
             System.out.println((String) in.readObject());
-            System.out.println("Got it!");
+            System.out.println("Got it!");*/
             socket.close();
         } catch (Exception e) {
             System.out.println(e);
--- ClientHandler.java
+++ ClientHandler.java
@@ -1,9 +1,13 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.sql.*;
 
 public class ClientHandler implements Runnable{
     private Socket socket;
+    ArrayList UN = new ArrayList<>();
+    //clientData Clientdata;
+    String[] arr;
     public ClientHandler(Socket s){
         this.socket = s;
     }
@@ -14,14 +18,57 @@ public class ClientHandler implements Runnable{
             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            //PrintWriter w = new PrintWriter(socket.getOutputStream(), true);
             String name = "dump.txt";
+            // try{
+            //     name = (String) in.readObject();
+            // }
+            // catch(Exception e){
+            //     e.printStackTrace();
+            // }
+            String userName = "";
+
             try{
-                name = (String) in.readObject();
+                // Clientdata =  (clientData) in.readObject();
+                arr = (String[]) in.readObject();
             }
             catch(Exception e){
                 e.printStackTrace();
             }
-            File file = new File("D:\\CN\\Project\\"+name);
+
+            try {
+
+                // Connection connection = DriverManager.getConnection(
+                //         "jdbc:sqlserver://DESKTOP-TA4RQON:1433;databaseName=Resort_DB;userName=admin;password=123;trustServerCertificate=true");
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:sqlserver://Hareem:1433;databaseName=CloudStorage;userName=hareem123;password=12345;trustServerCertificate=true");
+                
+                PreparedStatement statement = connection.prepareStatement("select UserName from Client");
+            
+                ResultSet result = statement.executeQuery();
+                
+                while (result.next()) {
+                    
+                    UN.add(result.getString("UserName").trim());
+                }
+                //System.out.println(UN);
+                if(UN.contains(arr[0])) {
+                    System.out.println("UserName already exists");
+                }
+                else {
+                    int clientstat=0;
+                    int TotalSpace=0;
+                    int occSpace=0;
+                    PreparedStatement statement1 = connection.prepareStatement("Insert into Client values ('"+arr[0]+"','"+arr[1]+"','"+arr[2]+" ','"+arr[3]+"','"+clientstat+"','"+TotalSpace+"', "+occSpace+")");
+                    System.out.println("User addded");
+                }   
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            /*File file = new File("F:\\CloudStorage\\mssql-jdbc-10.2.1.jre8");
 
 
             FileOutputStream fout = new FileOutputStream(file);
@@ -36,10 +83,10 @@ public class ClientHandler implements Runnable{
             System.out.println("Files");
             String str = "File Recieved!";
             out.writeObject(str);
-            System.out.println("Files2");
+            System.out.println("Files2");*/
             out.close();
             dout.close();
-            fout.close();
+            //fout.close();
             din.close();
             socket.close();
         
